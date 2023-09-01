26994573da14c5fe5b0b8a98d9dba14e1243445f
Leonardo Lamoia <leonardotcinf@gmail.com>
2023-09-01 00:12:50 -0300

    initial project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Leonardo Lamoia on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
